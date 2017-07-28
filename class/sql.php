e3889fe9bc41389986ca0fc75b91259264c2821f
Sílvio Dias <sildias15@gmail.com>
2017-07-28 23:48:27 +0100

    Projecto
    iniciação do projecto

---
+++ class/sql.php
@@ -0,0 +1,55 @@
+<?php
+
+class Sql extends PDO {
+
+	private $conn;
+
+	public function __construct(){
+
+
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+
+	} 				// Fim Function __construct
+
+
+private function setParams($statment, $parameters = array()){
+
+		foreach ($parameters as $key => $value) {
+		
+			$this->setParam($key, $value);
+
+		}
+	} 			// Fim function setParams
+
+private function setParam($statment, $key, $value){
+
+		$statment->bindParam($key, $value);
+
+	}				//Fim function setParam
+
+public function query($rawQuery, $params = array()){
+
+		$stmt = $this->conn->prepare($rawQuery);
+			
+			$this->setParams($stmt, $params);
+
+			$stmt->execute();
+
+			return $stmt; 
+		
+	}				//Fim function query
+
+
+public function select($rawQuery, $params = array()):array
+	{
+
+	$stmt =	$this->query($rawQuery, $params);
+
+	return $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+ }					//Fim function select
+
+
+} 					//Fim Class sql
+
+?>
